784e1547c7ff7e370fb57e9953fa753540891f03
Pikachu15103123 <2637206895@qq.com>
2017-08-21 15:28:41 +0800

    第一次更新·
    一个几乎是空白的java项目

---
+++ JavaWorkSpace/src/com/bjut/Test01/Demo01.java
@@ -0,0 +1,11 @@
+package com.bjut.Test01;
+
+/**
+ * Created by lenovo on 2017/8/21.
+ */
+public class Demo01 {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+
+}
