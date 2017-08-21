03d33b3276434e8af8e06210bfa9381dd7b16f10
jklmaker <956970564@qq.com>
2017-08-21 15:46:18 +0800

    第二次提交
    添加了Shape类
    添加了Circle类

---
+++ JavaWorkSpace/src/com/bjut/Test01/Circle.java
@@ -0,0 +1,7 @@
+package com.bjut.Test01;
+
+/**
+ * Created by LENOVO on 2017/8/21.
+ */
+public class Circle extends Shape {
+}
---
+++ JavaWorkSpace/src/com/bjut/Test01/Shape.java
@@ -0,0 +1,8 @@
+package com.bjut.Test01;
+
+/**
+ * Created by LENOVO on 2017/8/21.
+ */
+public class Shape {
+
+}
